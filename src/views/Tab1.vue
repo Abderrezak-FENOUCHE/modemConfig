9af5eaa9e26b9b4480483d59a9832c9de6590379
Abderrezak-FENOUCHE <abfenouche@gmail.com>
2021-06-24 12:22:50 +0200

    home skelton

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -2,27 +2,107 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Home</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <ion-grid>
+        <ion-row>
+          <ion-col class="ion-text-center" size="4">
+            <ion-icon :icon="wifi" />
+          </ion-col>
+          <ion-col class="ion-text-center" size="4">
+            <ion-row>
+              <ion-col size="12">
+                Etat wifi
+              </ion-col>
+              <ion-col size="12">
+                0.0.0.0
+              </ion-col>
+              <ion-col size="12">
+                192.168.5.3
+              </ion-col>
+            </ion-row>
+          </ion-col>
+          <ion-col class="ion-text-center" size="4">
+             {{battery}} %
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col class="ion-text-center" size="4">
+            <ion-icon name="add-circle-outline"></ion-icon>
+            Mhz
+          </ion-col>
+          <ion-col class="ion-text-center" size="4">
+                <ion-icon :icon="wifi" />
+          </ion-col>
+          <ion-col class="ion-text-center" size="4">
+            mbps
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col class="ion-text-center" size="4">
+            <ion-icon name="add-circle-outline"></ion-icon>
+            <ion-icon name="md-add"></ion-icon>
+            Paramètre wifi
+          </ion-col>
+          <ion-col class="ion-text-center" size="4">
+                Les Mot de passe
+          </ion-col>
+          <ion-col class="ion-text-center" size="4">
+            Scanner Wifi
+          </ion-col>
+        </ion-row>
+        <ion-row>
+          <ion-col class="ion-text-center" size="6">
+            <ion-icon name="add-circle-outline"></ion-icon>
+            Aidez-moi
+          </ion-col>
+          <ion-col class="ion-text-center" size="6">
+            Ping
+          </ion-col>
+        </ion-row>
+        
+      </ion-grid>
     </ion-content>
   </ion-page>
+  
 </template>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonIcon,
+  IonCol,
+  IonGrid,
+  IonRow,
+} from "@ionic/vue";
+import { wifi } from "ionicons/icons";
+export default {
+  name: "Tab1",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonIcon,
+    IonCol,
+    IonGrid,
+    IonRow,
+  },
+ data: () => ({
+    battery : 50
+  }),
 
-export default  {
-  name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+  setup() {
+    return {
+      wifi
+    };
+  },
+};
 </script>
